22e411dc1b4ad8f77a0304ed4ed937a8d83adcfd
Kris <kris@viirose.com>
2025-01-01 16:56:44 +0800

    init

--- run.sh
+++ run.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
 # bios_id=$(sudo dmidecode -s system-serial-number)
+terminal_id=1
+line="a"
+unit_id=1
 
-sudo cp logo_start.png 
+hostname="terminal-wl-${line}-${unit_id}"
+
+sudo cp logo_start.png /usr/share/plymouth/themes/spinner/watermark.png 
 
 sudo apt update
 sudo apt install -y vim git
 git config --global user.name "kris"
 git config --global user.email "kris@example.com"
 
+# sudo nmcli con mod wlp1s0 ipv4.method manual ipv4.addresses 172.16.22.$terminal_id/16 ipv4.gateway 172.16.0.1 ipv4.dns 172.16.0.1
+# sudo nmcli con down wlp1s0
+# sudo nmcli con up wlp1s0
 
+sudo dpkg -i winter_1.0.0_amd64.deb
 
+cat > /etc/systemd/system/winter.service  <<EOF
+[Unit]
+Description=Winter Application
+
+[Service]
+ExecStart=/usr/local/lib/winter/winter
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable winter.service
